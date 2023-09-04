fbb96aad03694fe2133417e0bdd3664fe5a1aa50
Keagan <keygun2k1@gmail.com>
2023-09-04 21:43:23 +0200

    Prevent bigger then 2MB images

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -11,6 +11,17 @@ class ImageUploadController
         if ($file === null) {
             return null;
         }
+        if ($file->getSize() > 2 * 1024 * 1024) {
+            $image = Image::make($file);
+            $image->resize(800, null, function ($constraint) {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            });
+            $webpFileName = uniqid().'.'.'webp';
+            $image->encode('webp', 80);
+            $image->save(public_path('images/'.$webpFileName));
+            return 'images/'.$webpFileName;
+        }
         $webpFileName = uniqid().'.'.'webp';
         $image = Image::make($file);
         $image->encode('webp', 80);
